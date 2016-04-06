8476ee231a293524f286dc2f15522456d1025c30
Mike Francis <mikeffrancis@gmail.com>
2016-04-06 14:21:00 +0100

    Initial commit

---
+++ content.js
@@ -0,0 +1,21 @@
+var keyCode = 191;
+
+window.addEventListener('keydown', function (event) {
+	if (event.keyCode !== keyCode) {
+  	return;
+  }
+
+  event.preventDefault();
+
+	var form = document.forms[0];
+
+  if (typeof form === 'undefined') {
+    return;
+  }
+
+  [].forEach.call(form.elements, function (element) {
+    if (element.type === 'text' || element.type === 'email') {
+      return element.focus();
+    }
+  });
+});
